4fb7a4c7bf5fa47ad60a75ec82ae7b7b6d805b5b
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2020-10-09 02:33:33 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
 
+games = ["mario", "tetris", "sonic", "zelda"]
 
+def my_collect(games) 
+    i = 0
+    game_list = []
+    while i < games.length
+        game_list << yield(games[i])
+        i = i + 1
+    end 
+game_list
+
+end
+
+my_collect(games) { |i| i.upcase}
